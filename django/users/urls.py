a3510d8f421577cfd884bf461746789bbf425aef
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-11-12 09:30:08 -0500

    django: CRUD operations done!

--- django/users/urls.py
+++ django/users/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('create/', views.create_user),
-    path('read/', views.read_user),
-    path('update/', views.update_user),
-    path('delete/', views.delete_user),
+    path('read/', views.read_users),
+    path('update/<int:user_id>', views.update_user),
+    path('delete/<int:user_id>', views.delete_user),
 ]
--- django/users/views.py
+++ django/users/views.py
@@ -2,39 +2,53 @@
 from django.http import JsonResponse
 from urllib.parse import unquote
 import json
+from .models import User
 
 # Create your views here.
 
 
+def body_parser(body):
+    data = body
+    data = str(data, encoding='utf-8')
+    data = unquote(data)
+    if data.rindex(',') >= len(data)-3:
+        data = data[:data.rindex(',')] + data[data.rindex(',')+1:]
+    return json.loads(data)
+
+
 def create_user(request):
-    # try:
-    if request.method == "POST":
-        data = request.body
-        data = str(data, encoding='utf-8')
-        data = unquote(data)
-        if data.rindex(',') <= len(data)-3:
-            data = data[:data.rindex(',')] + data[data.rindex(',')+1:]
-        data = json.loads(data)
-        print(data)
+    try:
+        if request.method == "POST":
+            data = body_parser(request.body)
+            User.objects.create(**data)
 
-    return JsonResponse({'msg': 'created'})
-    # except:
-    #     return JsonResponse({'msg': 'wrong input'})
+            return JsonResponse({'msg': 'created'})
+        else:
+            return JsonResponse({'msg': 'wrong method'})
+
+    except:
+        return JsonResponse({'msg': 'wrong input'})
 
 
-def read_user(request):
+def read_users(request):
     if request.method == "GET":
-        print(request.method)
-    return JsonResponse({'msg': 'read'})
+        return JsonResponse(list(User.objects.values()), safe=False)
+    else:
+        return JsonResponse({'msg': 'wrong method'})
 
 
-def update_user(request):
-    if request.method == "UPDATE":
-        print(request.method)
-    return JsonResponse({'msg': 'update'})
+def update_user(request, user_id):
+    if request.method == "PUT":
+        data = body_parser(request.body)
+        User.objects.filter(id=user_id).update(**data)
+        return JsonResponse({'msg': 'update'})
+    else:
+        return JsonResponse({'msg': 'wrong method'})
 
 
-def delete_user(request):
+def delete_user(request, user_id):
     if request.method == "DELETE":
-        print(request.method)
-    return JsonResponse({'msg': 'delete'})
+        User.objects.filter(id=user_id).delete()
+        return JsonResponse({'msg': 'delete'})
+    else:
+        return JsonResponse({'msg': 'wrong method'})
