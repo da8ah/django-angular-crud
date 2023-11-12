dd47c7e63397510d3dab30b9544d07c789252f1c
Tiber <danilo.ochoa.hidalgo@outlook.com>
2023-11-12 13:43:18 -0500

    angular: simple form

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular';
+
+  onSubmit(form: NgForm) {
+    console.log(form)
+  }
 }
